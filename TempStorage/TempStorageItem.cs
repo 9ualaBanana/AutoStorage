1925eddd7d2241484c8a1d1a9824125409e62a63
GIBSON <9ualabanana@gmail.com>
2022-08-24 19:16:23 +0300

    Add TempStorageItem<T>.

---
+++ TempStorage/TempStorageItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Timers;
+
+namespace System.Collections.Specialized;
+
+public class TempStorageItem<T> : IEquatable<TempStorageItem<T>>
+{
+    public readonly T Value;
+    public readonly GTimer Timer;
+
+
+    public TempStorageItem(T value, GTimer timer)
+    {
+        Value = value;
+        Timer = timer;
+    }
+
+    public TempStorageItem(KeyValuePair<T, GTimer> pair) : this(pair.Key, pair.Value)
+    {
+    }
+
+
+    public override bool Equals(object? obj) => Equals(obj as TempStorageItem<T>);
+    public bool Equals(TempStorageItem<T>? other)
+    {
+        if (other is null) return false;
+        return Value is null ? other.Value is null : Value.Equals(other.Value);
+    }
+    public override int GetHashCode() => Value is null ? 0 : Value.GetHashCode();
+
+
+    public static implicit operator TempStorageItem<T>(KeyValuePair<T, GTimer> pair) =>
+        new(pair.Key, pair.Value);
+    public static implicit operator KeyValuePair<T, GTimer>(TempStorageItem<T> this_) =>
+        new(this_.Value, this_.Timer);
+}
